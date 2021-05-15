607901e9f3fbaaeca5ee88055e5886611df124cd
Alan Oliveira <alancjobm@gmail.com>
2021-05-15 16:53:04 -0300

    feat: add static files

--- estoque/settings.py
+++ estoque/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
     'core',
     'produto',
     'entrada'
@@ -109,3 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static')
+]
+
